f458bd9a11c2d41eb65285ec66e31e61921030ce
Balduino Fernando <bafernando94@gmail.com>
2022-06-27 13:44:49 +0100

    Fix: TasksController

--- src/controllers/TasksController.ts
+++ src/controllers/TasksController.ts
@@ -1,7 +1,3 @@
-
-class TasksController {
-  getTasks(request: Request, response) {}
-  addTask(task: any) {}
-  updateTask(task: any) {}
-  deleteTask(task: any) {}
+export class TasksController {
+  
 }
